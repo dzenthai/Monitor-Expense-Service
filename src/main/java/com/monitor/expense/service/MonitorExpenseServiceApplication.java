0c3e62e3e64e249d154a284d6e99b2e36e4f435e
Dzenthai <dzenthai@proton.me>
2024-08-05 17:58:01 +0500

    Initial commit

---
+++ src/main/java/com/monitor/expense/service/MonitorExpenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.monitor.expense.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorExpenseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorExpenseServiceApplication.class, args);
+    }
+
+}
